35fb17192735a5a9834cd7cfc5cb455db86a6bab
johannes-p <83518424+johannes-p@users.noreply.github.com>
2023-08-24 00:24:07 +0200

    add basic terrain randomization, add ui depth display, test slider styling

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
+signal update_depth()
+
+@export var camera_node : Camera2D = null
+
+@onready var screensize : Vector2 = DisplayServer.window_get_size()
 
 const SPEED : float = 300.0
 const ROTATION_AMOUNT : int = 2
-const FRICTION : float = 0.025
-
+const FRICTION : float = 0.015
 
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
@@ -20,3 +24,8 @@ func _physics_process(delta):
 		$AnimationPlayer.current_animation = 'idle'
 
 	move_and_slide()
+	
+	camera_node.position.y = position.y
+	position.x = clamp(position.x, 0, screensize.x)
+	
+	emit_signal("update_depth", int((position.y / (270.0 / 1.80))))
---
+++ scripts/world.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+@export var terrain_tiles : Array[CompressedTexture2D] = []
+@export var terrain_scene : PackedScene = null
+@export var terrain_depth : int = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	
+	var left_starting_position = $terrain_start_l.position
+	var right_starting_position = $terrain_start_r.position
+	
+	for index in range(terrain_depth):
+		
+		# instanciate terrain 'object'
+		var left_instance = terrain_scene.instantiate()
+		var right_instance = terrain_scene.instantiate()
+		
+		
+		# set random sprite
+		var sprite_index = randi() % len(terrain_tiles)
+		left_instance.texture = terrain_tiles[sprite_index]
+		
+		sprite_index = randi() % len(terrain_tiles)
+		right_instance.texture = terrain_tiles[sprite_index]
+		
+		
+		# position the terrain - sprite height = 540px
+		left_instance.global_position = Vector2(left_starting_position.x, \
+		left_starting_position.y + (index * 540))
+		
+		right_instance.global_position = Vector2(right_starting_position.x, \
+		right_starting_position.y + (index * 540))
+		
+		#flip sprite for right instance
+		right_instance.flip_h = true
+		
+		
+		add_child(left_instance)
+		add_child(right_instance)
